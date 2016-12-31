a33e105436745fd0c16b5e7500f2b37528125d79
Frankenstein91 <frankenstein91.info@googlemail.com>
2017-01-01 00:32:56 +0100

    Some changes :D

--- MetaGet/admin.py
+++ MetaGet/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from MetaGet.models import WebSite
+
+def scan_WebSite(modeladmin, request, queryset):
+    for obj in queryset:
+        obj.scan()
+
+class WebSiteAdmin(admin.ModelAdmin):
+    list_display = ['Url', 'LastScan']
+    list_filter = ['LastScan', 'Scanned']
+    # fields to search in change list
+    search_fields = ['Url', 'MD5Sum']
+    # enable the save buttons on top on change form
+    save_on_top = True
+    actions = [scan_WebSite]
+    
+admin.site.register(WebSite, WebSiteAdmin)
--- MetaGet/models.py
+++ MetaGet/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+import requests, hashlib
+import _hashlib
+import django
+from _datetime import datetime
 
 # Create your models here.
+class WebSite(models.Model):
+    Url = models.URLField(blank=False, null=False)
+    Scanned = models.BooleanField(null=False)
+    LastScan = models.DateTimeField(null=True, blank=True)
+    MD5Sum = models.CharField(max_length=32,null=True, blank=True)
+    encoding = models.CharField(max_length=15,null=True, blank=True)
+    ContentType = models.CharField(max_length=15,null=True, blank=True)
+    
+    
+    def scan(self):
+        site = requests.get(self.Url)
+        self.MD5Sum = hashlib.md5(site.text.encode(encoding='utf_8')).hexdigest()
+        self.LastScan = datetime.now()
+        self.Scanned=True
+        self.encoding = site.encoding
+        self.ContentType = site.headers["Content-Type"]
+        self.save()
+        
+        
+        
